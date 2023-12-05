2d010ed2f9d96ee5659161e14dae726953191f20
Aiden Krecker <aiden.krecker@web.de>
2023-12-05 14:17:32 +0100

    🐋 Clean Working Env

---
+++ helloworld.js
@@ -0,0 +1,9 @@
+//ASI: Automatic Semicolon Insertion
+//Define a variable for the greeting program
+let greeting = "Hello";
+
+//Define a variable for the target of greeting
+let target = "World";
+
+//Combine the Variables and Output the Result
+console.log(greeting + " " + target + "!");
